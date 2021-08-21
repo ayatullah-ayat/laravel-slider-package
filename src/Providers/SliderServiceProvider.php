5734620456f6432a640a929175890f9157c3dab7
Ayatullah Khameni <ayat2486@gmail.com>
2021-08-21 10:59:32 +0600

    Added my version

--- src/Providers/SliderServiceProvider.php
+++ src/Providers/SliderServiceProvider.php
@@ -1,19 +1,23 @@
-<?php 
+<?php
 
 namespace Ponditcommerce\Slider\Providers;
+
 use Illuminate\Support\ServiceProvider;
 
+use Ponditcommerce\Slider\Slider;
 
-class SliderServiceProvider extends ServiceProvider{
-
-    public function register(){
-
-        
-
+class SliderServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register a class in the service container
+        // $this->app->bind('slider', function ($app) {
+        //     return new Slider();
+        // });
+        $this->app->bind('slider', \Ponditcommerce\Slider\Slider::class);
     }
 
-    public function boot(){
-
+    public function boot()
+    {
     }
-
 }
